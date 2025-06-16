63307607acc6f966c0711fdde0168f9573ce2491
markian mwangi <50865281+markmumba@users.noreply.github.com>
2025-06-16 12:31:31 +0300

    added test for the movie service

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    testMatch: [
+        '**/?(*.)+(test).[tj]s?(x)'
+    ],
+}; 
